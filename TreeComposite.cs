35fecc033f70f846cfe1b6c6859f541abfe483a1
Sandhya0824 <sandhyaa246789@gmail.com>
2023-08-03 21:55:22 +0530

    new committment added

---
+++ TreeComposite.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePatternTree
+{
+    internal class File : IFileSystemComponent
+    {
+        private string name;
+        public File(string path)
+        {
+            name = System.IO.Path.GetFileName(path);
+        }
+        public string GetName()
+        {
+            return name;
+        }
+        public int CountFileElements()
+        {
+            return 1;
+        }
+        public int CountFolderElements()
+        {
+            return 1;
+        }
+    }
+}
